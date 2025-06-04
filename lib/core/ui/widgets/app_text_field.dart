722878c0cc21ae032983b4d563112f75db93b651
ahmedabdalla67 <ahmedabdalla6728@gmail.com>
2025-06-04 16:31:09 +0300

    - start on routine ui create fields and common Text field

---
+++ lib/core/ui/widgets/app_text_field.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../color_palette.dart';
+import '../text_styles.dart';
+
+class AppTextField extends StatelessWidget {
+  final EdgeInsetsGeometry? contentPadding;
+  final InputBorder? enabledBorder;
+  final InputBorder? focusedBorder;
+  final InputBorder? errorBorder;
+  final InputBorder? focusedErrorBorder;
+  final TextStyle? inputTextSyle;
+  final TextStyle? hintStyle;
+  final String hintText;
+  final Color? backGroundColor;
+  final Widget? prefixIcon;
+  final Function()? onTap;
+  final TextEditingController? controller;
+  // final TextEditingController? controller;
+  const AppTextField({
+    super.key,
+    this.contentPadding,
+    this.enabledBorder,
+    this.focusedBorder,
+    this.inputTextSyle,
+    this.hintStyle,
+    required this.hintText,
+    this.backGroundColor,
+    this.errorBorder,
+    this.focusedErrorBorder,
+    this.prefixIcon,
+    this.onTap, this.controller,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      style: TextStyles.body,
+      onTap: onTap,
+      decoration: InputDecoration(
+        contentPadding:
+            contentPadding ??
+            EdgeInsets.symmetric(horizontal: 20.w, vertical: 18.h),
+        focusedBorder:
+            focusedBorder ??
+            OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16),
+              borderSide: BorderSide(width: 1.3, color: Palette.textPrimary),
+            ),
+        enabledBorder:
+            enabledBorder ??
+            OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16),
+              borderSide: BorderSide(width: 1.3, color: Palette.textLight),
+            ),
+        hintStyle:
+            hintStyle ?? TextStyles.body.copyWith(color: Color(0xFFC2C2C2)),
+        hintText: hintText,
+        fillColor: backGroundColor ?? Color(0xfffdfdff),
+        filled: true,
+        prefixIcon: prefixIcon,
+      ),
+    );
+  }
+}
---
+++ lib/features/routine/ui/widgets/date_text_field.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:wellness/core/ui/widgets/app_text_field.dart';
+
+class DateTextField extends StatefulWidget {
+  const DateTextField({super.key});
+
+  @override
+  State<DateTextField> createState() => _DateTextFieldState();
+}
+
+class _DateTextFieldState extends State<DateTextField> {
+  final TextEditingController _dateController = TextEditingController();
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2222),
+    );
+    if (picked != null) {
+      setState(() {
+        _dateController.text = DateFormat('dd-MM-yyyy').format(picked);
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _dateController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AppTextField(hintText: 'Select Date', prefixIcon: Icon( Icons.calendar_month), controller: _dateController, onTap: ()=> _selectDate(context),);
+  }
+}
